f64ef9961463fd5aba127667cd38a2997af85b9a
chronorose <rendemare@gmail.com>
2026-02-13 01:06:21 +0700

    le hanoi

--- src/Task3.hs
+++ src/Task3.hs
@@ -1,4 +1,5 @@
 {-# OPTIONS_GHC -Wall #-}
+
 -- Note: the above pragma enables all warnings
 
 module Task3 where
@@ -7,6 +8,7 @@ module Task3 where
 -- Helper type synonyms
 
 type Peg = String
+
 type Move = (Peg, Peg)
 
 -----------------------
@@ -17,5 +19,5 @@ type Move = (Peg, Peg)
 -- [("a","c"),("a","b"),("c","b")]
 
 hanoi :: Int -> Peg -> Peg -> Peg -> [Move]
-hanoi = error "TODO: define hanoi"
-
+hanoi 0 _ _ _ = []
+hanoi i a b c = hanoi (i - 1) a c b ++ (a, b) : hanoi (i - 1) c b a
